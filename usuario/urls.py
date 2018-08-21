0f72889d0c06d4b97282dadf7963657bc86aa4fd
junica <freesoftandskate@gmail.com>
2018-08-20 20:52:03 -0500

    mejorando el performance de la app

--- usuario/urls.py
+++ usuario/urls.py
@@ -4,5 +4,6 @@ from .views import primeravista
 urlpatterns = [
 
     path('listarusuario', primeravista),
+    path('listarusuario2', primeravista),
                  # path('admin/', admin.site.urls),
               ] 
